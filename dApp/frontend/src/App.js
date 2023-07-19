fed05ff59f7903ef744abe4bd2785c74c3d874d9
Love Nakagawa <love.nakagawa@gmail.com>
2023-07-19 19:55:28 +0800

    Attempt to connect Anchor Program to frontend

--- dApp/frontend/src/App.js
+++ dApp/frontend/src/App.js
@@ -1,8 +1,7 @@
+import * as anchor from '@project-serum/anchor';
 import { WalletAdapterNetwork, WalletNotConnectedError } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { ConnectionProvider, WalletProvider, useConnection, useWallet, useAnchorWallet } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-// import { Button } from '@solana/wallet-adapter-react-ui/lib/types/Button';
-
 import '../src/css/bootstrap.css'
 import {
     PhantomWalletAdapter,
@@ -10,46 +9,30 @@ import {
     SolletWalletAdapter,
 
 } from '@solana/wallet-adapter-wallets';
-import fs from "fs";
-
-import { clusterApiUrl, Transaction, SystemProgram, Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
+// import fs from "fs";
+import { clusterApiUrl, Transaction, SystemProgram, Keypair, PublicKey } from '@solana/web3.js';
 import React, { FC, ReactNode, useMemo, useCallback, useState } from 'react';
-
 import { Connection} from '@metaplex/js'; 
-
-
+import idl from "./idl.json";
 
 require('./App.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
-let thelamports = 0;
-let theWallet = "9m5kFDqgpf7Ckzbox91RYcADqcmvxW4MmuNvroD5H2r9"
-function getWallet(){
 
-    
-}
-const App: FC = () => {
+const PROGRAM_KEY = new PublicKey(idl.metadata.address);
 
 
+const App = () => {
     return (
         <Context>
             <Content />
         </Context>
     );
 };
-
-
 export default App;
 
-const Context: FC<{ children: ReactNode }> = ({ children }) => {
-    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+const Context = ({ children }) => {
     const network = WalletAdapterNetwork.Devnet;
-
-    // You can also provide a custom RPC endpoint.
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-    // Only the wallets you configure here will be compiled into your application, and only the dependencies
-    // of wallets that your users connect to will be loaded.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]); // You can also provide a custom RPC endpoint.
     const wallets = useMemo(
         () => [
             new PhantomWalletAdapter(),
@@ -59,7 +42,18 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
         [network]
     );
 
-   
+    const anchorWallet = useAnchorWallet();
+    const { connection } = useConnection();
+    //const { publicKey } = useWallet();
+
+    const program = useMemo(() => {
+        if (anchorWallet) {
+        const provider = new anchor.AnchorProvider(connection, anchorWallet, anchor.AnchorProvider.defaultOptions())
+        return new anchor.Program(idl, PROGRAM_KEY, provider)
+        }
+    }, [connection, anchorWallet])
+
+    console.log(program,  "PROGRAM");
 
     return (
         <ConnectionProvider endpoint={endpoint}>
@@ -70,56 +64,36 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
     );
 };
 
-const Content: FC = () => {
-    let [lamports, setLamports] = useState(.1);
-    let [wallet, setWallet] = useState("9m5kFDqgpf7Ckzbox91RYcADqcmvxW4MmuNvroD5H2r9");
-
-  
-    
-
+const Content = () => {
     // const { connection } = useConnection();
     const connection = new Connection(clusterApiUrl("devnet"))
     const { publicKey, sendTransaction } = useWallet();
 
 
- 
-
+    // post text
     const onClick = useCallback( async () => {
 
-        if (!publicKey) throw new WalletNotConnectedError();
-        connection.getBalance(publicKey).then((bal) => {
-            console.log(bal/LAMPORTS_PER_SOL);
+        // if (!publicKey) throw new WalletNotConnectedError();
+        // connection.getBalance(publicKey).then((bal) => {
+        //     console.log(bal/LAMPORTS_PER_SOL);
 
-        });
+        // });
 
-        let lamportsI = LAMPORTS_PER_SOL*lamports;
-        console.log(publicKey.toBase58());
-        console.log("lamports sending: {}", thelamports)
-        const transaction = new Transaction().add(
-            SystemProgram.transfer({
-                fromPubkey: publicKey,
-                toPubkey: new PublicKey(theWallet),
-                lamports: lamportsI,
-            })
-        );
+        // let lamportsI = LAMPORTS_PER_SOL*lamports;
+        // console.log(publicKey.toBase58());
+        // console.log("lamports sending: {}", thelamports)
+        // const transaction = new Transaction().add(
+        //     SystemProgram.transfer({
+        //         fromPubkey: publicKey,
+        //         toPubkey: new PublicKey(theWallet),
+        //         lamports: lamportsI,
+        //     })
+        // );
 
-        const signature = await sendTransaction(transaction, connection);
+        // const signature = await sendTransaction(transaction, connection);
 
-        await connection.confirmTransaction(signature, 'processed');
+        // await connection.confirmTransaction(signature, 'processed');
     }, [publicKey, sendTransaction, connection]);
-
-    
-    // function setTheLamports(e: any)
-    // {
-    //     console.log(Number(e.target.value));
-    //     setLamports(Number(e.target.value));
-    //     lamports = e.target.value;
-    //     thelamports = lamports;
-    // }
-    // function setTheWallet(e: any){
-    //     setWallet(e.target.value)
-    //     theWallet = e.target.value;
-    // }
 
     return (
         <div className="App">
@@ -149,8 +123,8 @@ const Content: FC = () => {
                                 <p className="card-text">Sample text post lorem ipsum dolor amit</p>
                             </div>
                         </div>
+                        <hr />
                     </div>
-                    <hr />
 
                     <div className="col-sm-1">
                         <div className="card">
